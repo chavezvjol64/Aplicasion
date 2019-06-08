a0d261826a4c386b4ad6e6b217aea9463f736733
chavezvjol64 <51514441+chavezvjol64@users.noreply.github.com>
2019-06-08 11:17:41 -0700

    Add files via upload

---
+++ Aplicasion1/Aplicasion1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Aplicasion1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Boton1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Boton1
+            // 
+            this.Boton1.Location = new System.Drawing.Point(219, 147);
+            this.Boton1.Name = "Boton1";
+            this.Boton1.Size = new System.Drawing.Size(329, 155);
+            this.Boton1.TabIndex = 0;
+            this.Boton1.Text = "PRECIONAME U.U";
+            this.Boton1.UseVisualStyleBackColor = true;
+            this.Boton1.Click += new System.EventHandler(this.Boton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(284, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Preciona el Boton >:v";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Boton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Boton1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
